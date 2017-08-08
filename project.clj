295089b535bc4507e6bd3480b9a13a86d1866464
bellp <bellp@outlook.com>
2017-08-07 21:27:49 -0700

    Added -client parameter for JVM

--- project.clj
+++ project.clj
@@ -5,6 +5,7 @@
   :profiles {:dev {:dependencies [[midje "1.8.3"]]} :midje {}}
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :jvm-opts ["-server"]
 
   :dependencies [[org.clojure/clojure "1.8.0"]]
   :debug-repl {:resource-paths ["/Library/Java/JavaVirtualMachines/jdk1.8.0_66.jdk/Contents/Home/lib/tools.jar"]
